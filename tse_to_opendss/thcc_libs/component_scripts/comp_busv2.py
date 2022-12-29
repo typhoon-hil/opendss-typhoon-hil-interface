0e7f05d91d0de3d0e6b637d55c2b0a43eaa4c967
felipe.santos <felipe.santos@typhoon-hil.com>
2022-12-29 15:26:01 -0300

    - Removed external meters

--- tse_to_opendss/thcc_libs/component_scripts/comp_busv2.py
+++ tse_to_opendss/thcc_libs/component_scripts/comp_busv2.py
@@ -1,121 +1,3 @@
-def v_meas_changed(mdl, mask_handle, v_meas_on, created_ports=None):
-    """
-
-    :param mdl:
-    :param mask_handle:
-    :param v_meas_on:
-    :param created_ports:
-    :return:
-    """
-    comp_handle = mdl.get_parent(mask_handle)
-    phases = mdl.get_property_value(mdl.prop(mask_handle, "type"))
-
-    j1a = mdl.get_item(name="Aj", parent=comp_handle, item_type="junction")
-    j1b = mdl.get_item(name="Bj", parent=comp_handle, item_type="junction")
-    j1c = mdl.get_item(name="Cj", parent=comp_handle, item_type="junction")
-
-    for it in ["vAB_RMS", "vBC_RMS", "vCA_RMS", "verticalA", "verticalB", "verticalC"]:
-        handle = mdl.get_item(it, parent=comp_handle)
-        if handle:
-            mdl.delete_item(handle)
-
-    ja = mdl.get_item(name="jA_vmeas", parent=comp_handle)
-    jab = mdl.get_item(name="jAB_vmeas", parent=comp_handle)
-    jb = mdl.get_item(name="jB_vmeas", parent=comp_handle)
-
-    if "A" in phases:
-        mdl.create_connection(j1a, ja, name="verticalA")
-    if "B" in phases:
-        mdl.create_connection(j1b, jab, name="verticalB")
-    if "C" in phases:
-        mdl.create_connection(j1c, jb, name="verticalC")
-
-    meas_dict = {}
-
-    if v_meas_on == "True":
-        comp_type = "Voltage RMS"
-    else:
-        comp_type = "el_open"
-
-    if "AB" in phases:
-        meas_dict["vAB_RMS"] = {"pos": (7564, 8200), "comp_type": comp_type, "j1": ja, "j2": jab}
-    if "BC" in phases:
-        meas_dict["vBC_RMS"] = {"pos": (7684, 8200), "comp_type": comp_type, "j1": jab, "j2": jb}
-    if "ABC" in phases or "AC" in phases:
-        meas_dict["vCA_RMS"] = {"pos": (7624, 8400), "comp_type": comp_type, "j1": jb, "j2": ja, "flip": "flip_horizontal"}
-
-    for comp, props in meas_dict.items():
-        new_meas = mdl.create_component(props.get("comp_type"),
-                                        parent=comp_handle,
-                                        name=comp,
-                                        rotation=props.get("rot"),
-                                        flip=props.get("flip"),
-                                        position=(props.get("pos")[0], props.get("pos")[1])
-                                        )
-        mdl.create_connection(mdl.term(new_meas, "p_node"), props.get("j1"))
-        mdl.create_connection(mdl.term(new_meas, "n_node"), props.get("j2"))
-
-
-def i_meas_changed(mdl, mask_handle, i_meas_on, created_ports=None):
-    """
-
-    :param mdl:
-    :param mask_handle:
-    :param i_meas_on:
-    :param created_ports:
-    :return:
-    """
-    comp_handle = mdl.get_parent(mask_handle)
-    phases = mdl.get_property_value(mdl.prop(mask_handle, "type"))
-
-    j1a = mdl.get_item(name="Aj", parent=comp_handle)
-    j1b = mdl.get_item(name="Bj", parent=comp_handle)
-    j1c = mdl.get_item(name="Cj", parent=comp_handle)
-    j2a = mdl.get_item(name="A_imeas_j", parent=comp_handle)
-    j2b = mdl.get_item(name="B_imeas_j", parent=comp_handle)
-    j2c = mdl.get_item(name="C_imeas_j", parent=comp_handle)
-
-    for it in ["iA_RMS", "iB_RMS", "iC_RMS", "sc_imeas_A", "sc_imeas_B", "sc_imeas_C"]:
-        handle = mdl.get_item(it, parent=comp_handle)
-        if handle:
-            mdl.delete_item(handle)
-
-    if i_meas_on == "True":
-
-        meas_dict = {
-            f"iA_RMS": {"pos": (7792, 7856), "comp_type": "Current RMS", "j1": j1a, "j2": j2a},
-            f"iB_RMS": {"pos": (7792, 7952), "comp_type": "Current RMS", "j1": j1b, "j2": j2b},
-            f"iC_RMS": {"pos": (7792, 8048), "comp_type": "Current RMS", "j1": j1c, "j2": j2c}
-        }
-
-        for comp, props in meas_dict.items():
-            new_meas = mdl.create_component(props.get("comp_type"),
-                                            parent=comp_handle,
-                                            name=comp,
-                                            rotation=props.get("rot"),
-                                            flip=props.get("flip"),
-                                            position=(props.get("pos")[0], props.get("pos")[1])
-                                            )
-            mdl.create_connection(mdl.term(new_meas, "p_node"), props.get("j1"))
-            mdl.create_connection(mdl.term(new_meas, "n_node"), props.get("j2"))
-
-            # Create disabled if the phase is not available
-            if "A" not in phases and props.get("j1") == j1a:
-                mdl.disable_items(new_meas)
-            elif "B" not in phases and props.get("j1") == j1b:
-                mdl.disable_items(new_meas)
-            elif "C" not in phases and props.get("j1") == j1c:
-                mdl.disable_items(new_meas)
-
-    else:
-        if "A" in phases:
-            mdl.create_connection(j1a, j2a, name="sc_imeas_A")
-        if "B" in phases:
-            mdl.create_connection(j1b, j2b, name="sc_imeas_B")
-        if "C" in phases:
-            mdl.create_connection(j1c, j2c, name="sc_imeas_C")
-
-
 def circuit_dynamics(mdl, container_handle, caller_prop_handle=None, init=False):
     """
 
@@ -162,8 +44,6 @@ def circuit_dynamics(mdl, container_handle, caller_prop_handle=None, init=False)
         # Port Vars
         port_attributes = get_port_const_attributes(comp_type)
         create_delete_ports = [phase in comp_type for phase in ["A", "B", "C"]]
-        # External Meters Vars - I'll remove it in future
-        current_meas_handles = [mdl.get_item(name, parent=comp_handle) for name in ["iA_RMS", "iB_RMS", "iC_RMS"]]
 
         # Updating the Terminal Positions
         for cnt, handle in enumerate(comp_port_handles):
@@ -171,17 +51,16 @@ def circuit_dynamics(mdl, container_handle, caller_prop_handle=None, init=False)
                 mdl.set_port_properties(handle, terminal_position=port_attributes[comp_port_labels[cnt]]["term_pos"])
 
         if new_value == "On one side":
-            # delete all downstream ports
+            # delete all downstream ports and short circuit the meter ports
             for cnt, handle in enumerate(comp_port_handles):
                 if comp_port_handles[cnt]:
                     mdl.delete_item(comp_port_handles[cnt])
-                    # TODO: Remove the external measurements
-                    mdl.create_connection(meas_port_handles[cnt], mdl.term(current_meas_handles[cnt], "n_node"))
+                    mdl.create_connection(meas_port_handles[cnt], meas_port_handles[cnt+3])
         else:
             # Create downstream ports depending on comp_type
             for cnt, action in enumerate(create_delete_ports):
                 if not comp_port_handles[cnt] and action:
-                    mdl.delete_item(mdl.find_connections(mdl.term(current_meas_handles[cnt], "n_node"))[0])
+                    mdl.delete_item(mdl.find_connections(meas_port_handles[cnt+3])[0])
                     new_port = mdl.create_port(name=comp_port_labels[cnt],
                                                parent=comp_handle,
                                                kind="pe",
@@ -189,7 +68,12 @@ def circuit_dynamics(mdl, container_handle, caller_prop_handle=None, init=False)
                                                position=port_attributes[comp_port_labels[cnt]]["pos"],
                                                terminal_position=port_attributes[comp_port_labels[cnt]]["term_pos"],
                                                flip="flip_horizontal")
-                    mdl.create_connection(mdl.term(current_meas_handles[cnt], "n_node"), new_port)
+                    mdl.create_connection(meas_port_handles[cnt+3], new_port)
+
+        # Updating the icon (not mandatory)
+        mdl.refresh_icon(container_handle)
+        # Measurements Check (mandatory)
+        check_measurements(mdl, container_handle)
 
     # ------------------------------------------------------------------------------------------------------------------
     #  "type" property code
@@ -209,8 +93,6 @@ def circuit_dynamics(mdl, container_handle, caller_prop_handle=None, init=False)
         # Port Vars
         port_attributes = get_port_const_attributes(new_value)
         create_delete_ports = [phase in new_value for phase in ["A", "B", "C"]]
-        # External Meters Vars - I'll remove it in future
-        current_meas_handles = [mdl.get_item(name, parent=comp_handle) for name in ["iA_RMS", "iB_RMS", "iC_RMS"]]
 
         # Updating the Terminal Positions
         for cnt, handle in enumerate(comp_port_handles):
@@ -228,12 +110,11 @@ def circuit_dynamics(mdl, container_handle, caller_prop_handle=None, init=False)
                                                direction="in",
                                                position=port_attributes[comp_port_labels[cnt]]["pos"],
                                                terminal_position=port_attributes[comp_port_labels[cnt]]["term_pos"])
-                    # TODO: Remove the external measurements
                     mdl.create_connection(new_port, meas_port_handles[cnt])
                 # Downstream Logic
                 if not comp_port_handles[cnt+3]:
                     if conf == "On both sides":
-                        mdl.delete_item(mdl.find_connections(mdl.term(current_meas_handles[cnt], "n_node"))[0])
+                        mdl.delete_item(mdl.find_connections(meas_port_handles[cnt+3])[0])
                         new_port = mdl.create_port(name=comp_port_labels[cnt+3],
                                                    parent=comp_handle,
                                                    kind="pe",
@@ -241,7 +122,7 @@ def circuit_dynamics(mdl, container_handle, caller_prop_handle=None, init=False)
                                                    position=port_attributes[comp_port_labels[cnt+3]]["pos"],
                                                    terminal_position=port_attributes[comp_port_labels[cnt+3]]["term_pos"],
                                                    flip="flip_horizontal")
-                        mdl.create_connection(mdl.term(current_meas_handles[cnt], "n_node"), new_port)
+                        mdl.create_connection(meas_port_handles[cnt+3], new_port)
 
             else:
                 # Upstream Logic
@@ -250,8 +131,7 @@ def circuit_dynamics(mdl, container_handle, caller_prop_handle=None, init=False)
                 # Downstream Logic
                 if comp_port_handles[cnt+3]:
                     mdl.delete_item(comp_port_handles[cnt+3])
-                    # TODO: Remove the external rms meters from the schematic
-                    mdl.create_connection(meas_port_handles[cnt], mdl.term(current_meas_handles[cnt], "n_node"))
+                    mdl.create_connection(meas_port_handles[cnt], meas_port_handles[cnt+3])
 
         # Updating the icon
         mdl.refresh_icon(container_handle)
@@ -336,12 +216,17 @@ def mask_dialog_dynamics(mdl, container_handle, caller_prop_handle=None, init=Fa
                 [mdl.enable_property(prop) for prop in [i_rms_meas_prop, power_meas_prop]]
 
     # ------------------------------------------------------------------------------------------------------------------
-    #  "conf" property code
+    #  "type" property code
     # ------------------------------------------------------------------------------------------------------------------
     if caller_prop_handle == type_prop:
 
-        comp_conf = mdl.get_property_disp_value(conf_prop)
+        if len(new_value) == 1:
+            [mdl.set_property_disp_value(prop, True) for prop in [ground_prop]]
+            [mdl.disable_property(prop) for prop in [ground_prop]]
+        else:
+            [mdl.enable_property(prop) for prop in [ground_prop]]
 
+        """ There are some miss measurements depending on the comp_type
         if len(new_value) == 3:
             # Enable Line and rms properties
             [mdl.enable_property(prop) for prop in [v_line_inst_meas_prop, v_line_rms_meas_prop,
@@ -359,6 +244,7 @@ def mask_dialog_dynamics(mdl, container_handle, caller_prop_handle=None, init=Fa
                 [mdl.disable_property(prop) for prop in [ground_prop]]
             else:
                 [mdl.enable_property(prop) for prop in [ground_prop]]
+        """
 
 
 def define_icon(mdl, container_handle):
@@ -406,11 +292,11 @@ def get_port_const_attributes(comp_type):
         term_positions = [(0, 0), (0, 0), (0, 0), (0, 0), (-8.0, 0), (8.0, 0)]
 
     port_dict = {"A1": {"pos": (7400, 7856), "term_pos": term_positions[0]},
-                 "A2": {"pos": (7984, 7856), "term_pos": term_positions[1]},
+                 "A2": {"pos": (7672, 7856), "term_pos": term_positions[1]},
                  "B1": {"pos": (7400, 7952), "term_pos": term_positions[2]},
-                 "B2": {"pos": (7984, 7952), "term_pos": term_positions[3]},
+                 "B2": {"pos": (7672, 7952), "term_pos": term_positions[3]},
                  "C1": {"pos": (7400, 8048), "term_pos": term_positions[4]},
-                 "C2": {"pos": (7984, 8048), "term_pos": term_positions[5]},
+                 "C2": {"pos": (7672, 8048), "term_pos": term_positions[5]},
                  "GND": {"pos": (7552, 8144), "term_pos": [0, 16*len(comp_type)]}}
 
     return port_dict
@@ -445,24 +331,15 @@ def check_measurements(mdl, container_handle):
             mdl.set_property_value(mdl.prop(meter_handle, meter_props_dict["v_line_inst_meas"][cnt]), action)
         if mdl.get_property_disp_value(mdl.prop(container_handle, "v_phase_inst_meas")) is True:
             mdl.set_property_value(mdl.prop(meter_handle, meter_props_dict["v_phase_inst_meas"][cnt]), action)
-    # Three-Phase props
-    if comp_type == "ABC":
-        if mdl.get_property_disp_value(mdl.prop(container_handle, "i_rms_meas")) is True:
-            mdl.set_property_value(mdl.prop(meter_handle, meter_props_dict["i_rms_meas"][0]), True)
-        if mdl.get_property_disp_value(mdl.prop(container_handle, "v_line_rms_meas")) is True:
-            mdl.set_property_value(mdl.prop(meter_handle, meter_props_dict["v_line_rms_meas"][0]), True)
-        if mdl.get_property_disp_value(mdl.prop(container_handle, "v_phase_rms_meas")) is True:
-            mdl.set_property_value(mdl.prop(meter_handle, meter_props_dict["v_phase_rms_meas"][0]), True)
-        if mdl.get_property_disp_value(mdl.prop(container_handle, "freq_meas")) is True:
-            mdl.set_property_value(mdl.prop(meter_handle, meter_props_dict["freq_meas"][0]), True)
-        if mdl.get_property_disp_value(mdl.prop(container_handle, "power_meas")) is True:
-            mdl.set_property_value(mdl.prop(meter_handle, meter_props_dict["power_meas"][0]), True)
-    else:
-        [mdl.set_property_value(mdl.prop(meter_handle, prop), False) for prop in meter_props_dict["freq_meas"]]
-        [mdl.set_property_value(mdl.prop(meter_handle, prop), False) for prop in meter_props_dict["v_phase_rms_meas"]]
-        [mdl.set_property_value(mdl.prop(meter_handle, prop), False) for prop in meter_props_dict["v_line_rms_meas"]]
-        [mdl.set_property_value(mdl.prop(meter_handle, prop), False) for prop in meter_props_dict["i_rms_meas"]]
-        [mdl.set_property_value(mdl.prop(meter_handle, prop), False) for prop in meter_props_dict["power_meas"]]
 
-    # TODO: Logic for external meters (I'll remove it on next versions)
-
+    # Three-Phase props (There are some miss measurements depending on comp_type)
+    if mdl.get_property_disp_value(mdl.prop(container_handle, "i_rms_meas")) is True:
+        mdl.set_property_value(mdl.prop(meter_handle, meter_props_dict["i_rms_meas"][0]), True)
+    if mdl.get_property_disp_value(mdl.prop(container_handle, "v_line_rms_meas")) is True:
+        mdl.set_property_value(mdl.prop(meter_handle, meter_props_dict["v_line_rms_meas"][0]), True)
+    if mdl.get_property_disp_value(mdl.prop(container_handle, "v_phase_rms_meas")) is True:
+        mdl.set_property_value(mdl.prop(meter_handle, meter_props_dict["v_phase_rms_meas"][0]), True)
+    if mdl.get_property_disp_value(mdl.prop(container_handle, "freq_meas")) is True:
+        mdl.set_property_value(mdl.prop(meter_handle, meter_props_dict["freq_meas"][0]), True)
+    if mdl.get_property_disp_value(mdl.prop(container_handle, "power_meas")) is True:
+        mdl.set_property_value(mdl.prop(meter_handle, meter_props_dict["power_meas"][0]), True)
