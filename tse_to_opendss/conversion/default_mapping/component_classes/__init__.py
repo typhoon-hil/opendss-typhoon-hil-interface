ad8943455717aea2de4e4a0b2895182422fddaa4
felipe.santos <felipe.santos@typhoon-hil.com>
2023-02-27 10:02:56 -0300

    - Added PVSystem and XYCurve Classes to the __init__ file

--- tse_to_opendss/conversion/default_mapping/component_classes/__init__.py
+++ tse_to_opendss/conversion/default_mapping/component_classes/__init__.py
@@ -15,6 +15,8 @@ from .loadshape import LoadShape
 from .linecode import LineCode
 from .regcontrol import RegControl
 from .container import Container
+from .pvsystem import PVSystem
+from .xycurve import XYCurve
 
 
 
