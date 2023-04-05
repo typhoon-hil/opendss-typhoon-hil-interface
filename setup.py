e0d18bd135a8448726d9fa39cd7e64c5b1121f3d
Marcos Moccelini <marcos.moccelini@typhoon-hil.com>
2023-04-05 19:33:35 -0300

    Setup fix for image wheel Load fix (not working with container)

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='tse_to_opendss',
     version='0.4.0',
-    packages=find_packages(exclude=['dss_thcc_lib', 'tests', 'importer']),
+    packages=find_packages(exclude=['tests', 'importer']),
+    package_data={"images": [r'dss_thcc_lib/images/*.png']},
     install_requires=["opendssdirect.py==0.8.0", "dss-python==0.13.0"],
     url='https://www.typhoon-hil.com/',
     include_package_data=True,
