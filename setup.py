d1580cb3d6a0718599116a57c30bf5e2f40fb445
marcos.moccelini <marcos.moccelini@typhoon-hil.com>
2023-09-06 16:40:25 +0200

    Setup file update

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='tse_to_opendss',
-    version='0.4.0',
-    packages=find_packages(exclude=['tests', 'importer']),
+    version='0.5.0',
+    packages=find_packages(exclude=['tests']),
     package_data={"images": [r'dss_thcc_lib/images/*.png']},
     install_requires=["opendssdirect.py==0.8.0", "dss-python==0.13.0"],
     url='https://www.typhoon-hil.com/',
