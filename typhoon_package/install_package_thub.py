547278a36b794b0bd66b0318fb9a44f095b169f0
felipe.santos <felipe.santos@typhoon-hil.com>
2024-07-05 14:04:57 -0300

    - Debug Thub

--- typhoon_package/install_package_thub.py
+++ typhoon_package/install_package_thub.py
@@ -5,7 +5,7 @@ import os
 package_folder = os.path.dirname(os.path.abspath(__file__))
 os.chdir(package_folder)
 opendss_folder = os.path.join(package_folder, "../")
-thub_package_folder = os.path.join(package_folder, 'package')
+thub_package_folder = os.path.join(opendss_folder, 'package')
 
 all_packages = pkm.get_installed_packages()
 print(f"All installed packages: {all_packages}")
