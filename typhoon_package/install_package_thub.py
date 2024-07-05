56ee7e041667a7a523149ade412a10af5b70eda7
felipe.santos <felipe.santos@typhoon-hil.com>
2024-07-05 14:02:52 -0300

    - Debug Thub

--- typhoon_package/install_package_thub.py
+++ typhoon_package/install_package_thub.py
@@ -7,6 +7,13 @@ os.chdir(package_folder)
 opendss_folder = os.path.join(package_folder, "../")
 thub_package_folder = os.path.join(package_folder, 'package')
 
+all_packages = pkm.get_installed_packages()
+print(f"All installed packages: {all_packages}")
+
 if os.path.exists(thub_package_folder):
-        pkm.install_package(thub_package_folder)
-        mdl.reload_libraries()
+    pkm.install_package(thub_package_folder)
+    mdl.reload_libraries()
+    all_packages = pkm.get_installed_packages()
+    print(f"All installed packages: {all_packages}")
+else:
+    print("There is no package folder")
