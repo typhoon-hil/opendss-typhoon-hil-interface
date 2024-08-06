eeeb4349205f116a606e160c5045e113a8e536d1
felipe.santos <felipe.santos@typhoon-hil.com>
2024-08-06 09:41:08 -0300

    - Added function to remove previous library paths

--- typhoon_package/install_library_thub.py
+++ typhoon_package/install_library_thub.py
@@ -6,6 +6,12 @@ os.chdir(package_folder)
 opendss_folder = os.path.join(package_folder, "../")
 dss_tlib_folder = os.path.join(opendss_folder, "/dss_thcc_lib")
 
+# Get all current library paths and remove them
+old_paths = mdl.get_library_paths()
+for path in old_paths:
+    mdl.remove_library_path(path)
+
+# Add OpenDSS library path
 mdl.add_library_path(library_path=dss_tlib_folder, add_subdirs=True, persist=True)
 
 print(f"Reloading...")
