75bb66521593f0b88f398d0f72e5619d0ae79472
felipe.santos <felipe.santos@typhoon-hil.com>
2024-08-06 09:45:45 -0300

    - Added print functions to debug

--- typhoon_package/install_library_thub.py
+++ typhoon_package/install_library_thub.py
@@ -8,12 +8,16 @@ dss_tlib_folder = os.path.join(opendss_folder, "/dss_thcc_lib")
 
 # Get all current library paths and remove them
 old_paths = mdl.get_library_paths()
+print(f"Removing previous library paths...")
 for path in old_paths:
     mdl.remove_library_path(path)
+    print(f"{path} removed.")
 
 # Add OpenDSS library path
+print(f"Adding OpenDSS library path...")
 mdl.add_library_path(library_path=dss_tlib_folder, add_subdirs=True, persist=True)
+print(f"{dss_tlib_folder} Added.")
 
-print(f"Reloading...")
+print(f"Reloading Libraries...")
 mdl.reload_libraries()
 print(f"Reloaded.")
