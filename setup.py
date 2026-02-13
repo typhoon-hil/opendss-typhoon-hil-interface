be5310eadb988138f757be1377e2b3e26ce53fb4
Tiarles Moralles Guterres <tiarles.moralles@typhoon-hil.com>
2026-02-13 17:17:35 +0100

    Fix dss-python version requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.5.2',
     packages=find_packages(exclude=['tests', 'importer']),
     package_data={"images": [r'dss_thcc_lib/images/*.png']},
-    install_requires=["opendssdirect.py==0.9.4", "dss-python==0.14.4"],
+    install_requires=["opendssdirect.py==0.9.4", "dss-python==0.15.7"],
     url='https://www.typhoon-hil.com/',
     include_package_data=True,
     license='MIT',
