ae6052454d89943960dc39b405e339418e18d40e
Tiarles Moralles Guterres <tiarles.moralles@typhoon-hil.com>
2026-02-13 17:08:22 +0100

    Updating requirements to meet THCC ones

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='tse_to_opendss',
-    version='0.5.1',
+    version='0.5.2',
     packages=find_packages(exclude=['tests', 'importer']),
     package_data={"images": [r'dss_thcc_lib/images/*.png']},
-    install_requires=["opendssdirect.py==0.8.0", "dss-python==0.13.0"],
+    install_requires=["opendssdirect.py==0.8.4", "dss-python==0.14.4"],
     url='https://www.typhoon-hil.com/',
     include_package_data=True,
     license='MIT',
